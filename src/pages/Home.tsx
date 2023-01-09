0fd0dfcfa683365a2f2af61bdb8eb352f613ee4f
vjvl95 <vjvl95@naver.com>
2023-01-09 21:19:34 +0900

    feat: 라우팅 연결 위한 설정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import PashionCard from '../components/PashionCard';
+import PundingCard from '../components/PundingCard';
+export default function Home() {
+	return (
+		<>
+			<PashionCard />
+			<PundingCard />
+		</>
+	);
+}
