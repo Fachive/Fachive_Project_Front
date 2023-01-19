4e69a528141c6a5df2449e375d161007d4174592
vjvl95 <vjvl95@naver.com>
2023-01-19 11:44:01 +0900

    design: Fashion 레이아웃 완성

--- src/pages/Fashion/Fashion.tsx
+++ src/pages/Fashion/Fashion.tsx
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import category from '../../assets/category.png';
+
 const Fashion = () => {
+	const [filter, setFilter] = useState('');
+
+	const onClickHander = (e: any) => {
+		console.log(e.target.children);
+	};
 	return (
 		<ContainerDiv>
 			<CategoryDiv />
@@ -8,19 +15,75 @@ const Fashion = () => {
 				<SubtitleDiv>새로운 트렌드의 시작 😎</SubtitleDiv>
 				<HeadTitleDiv>먼 훗날 유행이 될 최고의 아이템을 찾아보세요.</HeadTitleDiv>
 			</TextDiv>
+
+			<SelectDiv>
+				<FilterBoxDiv onClick={(e) => onClickHander(e)}>
+					<FilterItemButton className="likes">추천순</FilterItemButton>
+					<FilterItemButton className="lately">최신순</FilterItemButton>
+					<FilterItemButton className="mypick">마이픽</FilterItemButton>
+				</FilterBoxDiv>
+				<DropBoxDiv>
+					계절:
+					<DropItemSelect>
+						<option value="">전체</option>
+						<option value="봄">봄</option>
+						<option value="여름">여름</option>
+						<option value="가을">가을</option>
+						<option value="겨울">겨울</option>
+					</DropItemSelect>
+					카테고리:
+					<DropItemSelect>
+						<option value="">전체</option>
+						<option value="봄">봄</option>
+						<option value="여름">여름</option>
+						<option value="가을">가을</option>
+						<option value="겨울">겨울</option>
+					</DropItemSelect>
+				</DropBoxDiv>
+			</SelectDiv>
 		</ContainerDiv>
 	);
 };
 
+export default Fashion;
+
+const SelectDiv = styled.div`
+	display: flex;
+	margin-top: 100px;
+	justify-content: space-between;
+	width: 100%;
+`;
+const FilterBoxDiv = styled.div``;
+const DropBoxDiv = styled.div`
+	font-weight: 600;
+`;
+const DropItemSelect = styled.select`
+	border: none;
+	margin-right: 30px;
+	font-weight: 600;
+	font-size: 16px;
+`;
+const FilterItemButton = styled.button`
+	width: 70px;
+	height: 20px;
+	font-weight: 800;
+	font-size: 18px;
+	border: none;
+	background-color: white;
+	cursor: pointer;
+	&:active {
+		color: green;
+	}
+`;
 const ContainerDiv = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
 	align-items: center;
 `;
 const HeadTitleDiv = styled.div`
 	font-weight: 800;
 	font-size: 32px;
+	margin-top: 20px;
 `;
 const CategoryDiv = styled.div`
 	margin: 100px 0px;
@@ -34,4 +97,3 @@ const SubtitleDiv = styled.div``;
 const TextDiv = styled.div`
 	text-align: center;
 `;
-export default Fashion;
