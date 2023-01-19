5a48e9c5ffe41a4a2569443503d1e7ca35cd93ba
vjvl95 <vjvl95@naver.com>
2023-01-19 10:01:50 +0900

    feat: Nav바 링크 연결

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import LogoImg from '../assets/Logo.png';
 
@@ -5,10 +6,16 @@ const Navbar = () => {
 	return (
 		<Container>
 			<NavContainerDiv>
-				<LogoSpan />
-				<NavItemDiv>패션픽업</NavItemDiv>
-				<NavItemDiv>펀딩</NavItemDiv>
-				<NavItemDiv>포트폴리오</NavItemDiv>
+				<LogoSpan></LogoSpan>
+				<NavItemSpan>
+					<StyledLink to="/fashion">패션픽업</StyledLink>
+				</NavItemSpan>
+				<NavItemSpan>
+					<StyledLink to="/funding">펀딩</StyledLink>
+				</NavItemSpan>
+				<NavItemSpan>
+					<StyledLink to="/portfolio">포트폴리오</StyledLink>
+				</NavItemSpan>
 			</NavContainerDiv>
 			<UserContainerDiv>
 				<UserItemDiv>로그인</UserItemDiv>
@@ -19,6 +26,10 @@ const Navbar = () => {
 	);
 };
 export default Navbar;
+const StyledLink = styled(Link)`
+	text-decoration-line: none;
+	color: black;
+`;
 const Container = styled.div`
 	width: 100%;
 	height: 70px;
@@ -41,10 +52,13 @@ const NavContainerDiv = styled.div`
 	display: flex;
 	gap: 15px;
 `;
-const NavItemDiv = styled.div``;
+const NavItemSpan = styled.span`
+	color: black;
+	text-decoration-line: none;
+`;
 const UserContainerDiv = styled.div`
 	display: flex;
 	gap: 15px;
 	padding-right: 20px;
 `;
-const UserItemDiv = styled.div``;
+const UserItemDiv = styled.span``;
