2a2683d476b0b28f96e3959df4373e7319faf6c1
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2023-01-19 21:59:39 +0900

    feat: useInput 훅 구현 input의 value를 하나하나 지정하지 않고 훅으로 만듬. 후에 사용이 크게 필요하지 않다면 삭제할 가능성 있음.

---
+++ src/hooks/useInput.tsx
@@ -0,0 +1,17 @@
+import { useCallback, useState, ChangeEvent } from 'react';
+
+type ReturnTypes = [
+	string,
+	(e: React.ChangeEvent<HTMLInputElement>) => void,
+	React.Dispatch<React.SetStateAction<string>>
+];
+
+const useInput = (initialData: string): ReturnTypes => {
+	const [value, setValue] = useState(initialData);
+	const handler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	}, []);
+	return [value, handler, setValue];
+};
+
+export default useInput;
