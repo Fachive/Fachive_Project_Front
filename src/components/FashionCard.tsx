5d3331435ba8028fb8f40ddfc9447cfd34b0336a
vjvl95 <vjvl95@naver.com>
2023-01-24 12:54:55 +0900

    refactor: 이미지 케러셀 구현

--- src/components/FashionCard.tsx
+++ src/components/FashionCard.tsx
@@ -1,16 +1,17 @@
 import styled from 'styled-components';
 import { BiLike } from 'react-icons/bi';
 import { IoEyeOutline } from 'react-icons/io5';
-import { CardRes } from '../pages/Fashion/Fashion';
+import { CardRes } from '../types/fashionPage';
 
 interface ResProps {
 	data: CardRes;
+	idx?: number;
 }
 
-const FashionCard = ({ data }: ResProps) => {
+const FashionCard = ({ data, idx }: ResProps) => {
 	return (
 		<Card>
-			<CardIMG src={data.postImageDto.fileURI}></CardIMG>
+			<CardDiv imgSrc={data.postImageDto.fileURI}></CardDiv>
 			<CardInfoBox>
 				<CardLeftInfo>
 					<Profile />
@@ -23,6 +24,7 @@ const FashionCard = ({ data }: ResProps) => {
 					{data.pickup > 10000 ? Math.trunc(data.pickup / 1000) + 'k' : data.pickup}
 					<IoEyeOutline style={{ marginLeft: '15px' }} size="22" />
 					{data.views > 10000 ? Math.trunc(data.views / 10000) + 'k' : data.views}
+					{idx}
 				</CardLeftRightInfo>
 			</CardInfoBox>
 		</Card>
@@ -39,7 +41,8 @@ const CardLeftInfo = styled.div`
 `;
 const Card = styled.div`
 	border-radius: 10px;
-	min-width: 250px;
+	min-width: 310px;
+	flex: none;
 `;
 const Designer = styled.span`
 	margin-left: 10px;
@@ -56,8 +59,13 @@ const CardInfoBox = styled.div`
 	display: flex;
 	justify-content: space-between;
 `;
-const CardIMG = styled.img`
-	object-fit: cover;
+const CardDiv = styled.div<{ imgSrc: string }>`
 	width: 100%;
+	height: 252px;
 	border-radius: 10px;
+	background-image: url(${(props) => props.imgSrc});
+	background-position: 50% 50%;
+	background-size: contain;
+	background-repeat: no-repeat;
+	flex: none;
 `;
