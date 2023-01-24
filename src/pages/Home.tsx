8be0e2db30561ff9537df264deca123c0d42f695
vjvl95 <vjvl95@naver.com>
2023-01-24 12:55:48 +0900

    feat: 데이터 받아서 이미지 케러셀 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,72 @@
 import styled from 'styled-components';
 import homeImg from '../assets/homeImg.png';
 import fundingImg from '../assets/fundingImg.png';
-
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+import { CardRes } from '../types/fashionPage';
+import FashionCard from '../components/FashionCard';
+import FundingCard from '../components/FundingCard';
 const Home = () => {
+	const [CardData, setCardData] = useState<Array<CardRes>[]>([]);
+
+	const getData = async () => {
+		const res1 = await axios.get(
+			'http://ec2-54-180-7-198.ap-northeast-2.compute.amazonaws.com:8080/fashionpickup/main/get'
+		);
+		setCardData((prev) => [...prev, res1.data.data]);
+
+		const res2 = await axios.get(
+			'http://ec2-54-180-7-198.ap-northeast-2.compute.amazonaws.com:8080/funding/mainPageGet'
+		);
+		setCardData((prev) => [...prev, res2.data.data]);
+	};
+	const [current, setCurrent] = useState(0);
+	const [style, setStyle] = useState({
+		marginLeft: `-${current * 2}0%`,
+	});
+	const imgSize = useRef(10);
+
+	const moveSlide = (i: number) => {
+		let nextIndex = current + i;
+		if (nextIndex === -1) return;
+		if (nextIndex < 0) nextIndex = imgSize.current - 1;
+		else if (nextIndex >= imgSize.current) return;
+
+		setCurrent(nextIndex);
+	};
+
+	useEffect(() => {
+		setStyle({ marginLeft: `-${current * 2}0%` });
+	}, [current]);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
 	return (
 		<Container>
 			<SlideImg />
 			<RecommendTitle>패카이브 추천 픽</RecommendTitle>
 
-			<FachiveRecommend></FachiveRecommend>
+			<FachiveRecommend>
+				<button onClick={() => moveSlide(-1)}>이전</button>
+				<RecommendFlexDiv style={style}>
+					{CardData[0]?.slice(0, 10).map((fashionData, idx) => {
+						return <FashionCard data={fashionData} idx={idx} />;
+					})}
+				</RecommendFlexDiv>
+				<button onClick={() => moveSlide(+1)}>다음</button>
+			</FachiveRecommend>
 			<RecommendButton>패션추천 바로 가기</RecommendButton>
 			<FundingImg />
 			<RecommendTitle>패카이브 추천 픽</RecommendTitle>
-			<FachiveRecommend></FachiveRecommend>
+			<FachiveRecommend>
+				<RecommendFlexDiv style={style}>
+					{CardData[1]?.slice(0, 10).map((fundingData) => {
+						return <FundingCard data={fundingData} />;
+					})}
+				</RecommendFlexDiv>
+			</FachiveRecommend>
 		</Container>
 	);
 };
@@ -27,6 +81,10 @@ const RecommendButton = styled.button`
 	font-size: 16px;
 	font-weight: 700;
 	cursor: pointer;
+`;
+const RecommendFlexDiv = styled.div`
+	display: flex;
+	gap: 35px;
 `;
 const RecommendTitle = styled.div`
 	margin-left: 10px;
@@ -50,10 +108,8 @@ const Container = styled.div`
 	flex-direction: column;
 `;
 const FachiveRecommend = styled.div`
-	display: flex;
-	overflow-x: scroll;
-	gap: 12px;
-	width: 100%;
+	overflow: hidden;
+	min-width: 1700px;
 `;
 const SlideImg = styled.img`
 	max-width: 100%;
