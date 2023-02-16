930032a243bd57140a626c875f516db9de261789
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2023-02-16 22:10:15 +0900

    feat: 유저 등록 api 연동 작업

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -15,3 +15,7 @@ export const fashionPickUpDetailApi22 = async () => {
 	const data = await customAxios.get('/fashionpickup/multiGet?postFashionEntityId=1');
 	return data;
 };
+export const signApi = async () => {
+	const data = await customAxios.post('http://ec2-54-180-7-198.ap-northeast-2.compute.amazonaws.com:8080/user/post?');
+	return data;
+};
