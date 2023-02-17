a63489dcc5cc35c14be17252fe1b81ed82345467
vjvl95 <vjvl95@naver.com>
2023-02-17 21:16:28 +0900

    refactor: 패션추천 바로가기 버튼 링크 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,16 +6,17 @@ import axios from 'axios';
 import { CardRes } from '../types/fashionPage';
 import FashionCard from '../components/FashionCard';
 import FundingCard from '../components/FundingCard';
+import { Link } from 'react-router-dom';
 const Home = () => {
 	const [FashionCardData, setFashionCardData] = useState<CardRes[]>([]);
 	const [FundingCardData, setFundingCardData] = useState<CardRes[]>([]);
 
 	const getData = async () => {
 		const res1 = await axios.get('http://ec2-54-180-7-198.ap-northeast-2.compute.amazonaws.com:8080/main/get/ten');
-		console.log(res1);
 		setFashionCardData(res1.data.fashionpickList);
 		setFundingCardData(res1.data.fundingList);
 	};
+
 	const [currentFashion, setCurrentFashion] = useState(0);
 	const [currentFunding, setCurrentFunding] = useState(0);
 
@@ -55,9 +56,6 @@ const Home = () => {
 	useEffect(() => {
 		getData();
 	}, []);
-	useEffect(() => {
-		console.log(FashionCardData);
-	}, [FashionCardData]);
 
 	return (
 		<Container>
@@ -65,8 +63,8 @@ const Home = () => {
 			<ButtonDiv>
 				<RecommendTitle>패카이브 추천 픽</RecommendTitle>
 				<div>
-					<ButtonItemButton onClick={() => moveSlideFashion(-1)}>이전</ButtonItemButton>
-					<ButtonItemButton onClick={() => moveSlideFashion(+1)}>다음</ButtonItemButton>
+					<ButtonItemButton onClick={() => moveSlideFashion(-1)}>&lt;</ButtonItemButton>
+					<ButtonItemButton onClick={() => moveSlideFashion(+1)}>&gt;</ButtonItemButton>
 				</div>
 			</ButtonDiv>
 
@@ -77,13 +75,19 @@ const Home = () => {
 					})}
 				</RecommendFlexDiv>
 			</FachiveFashionRecommend>
-			<RecommendButton>패션추천 바로 가기</RecommendButton>
+
+			<RecommendButton>
+				<Link style={{ textDecorationLine: 'none', color: 'black' }} to="/fashion">
+					패션추천 바로 가기
+				</Link>
+			</RecommendButton>
+
 			<FundingImg />
 			<ButtonDiv>
 				<RecommendTitle>곧 마감되는 펀딩</RecommendTitle>
 				<div>
-					<ButtonItemButton onClick={() => moveSlideFunding(-1)}>이전</ButtonItemButton>
-					<ButtonItemButton onClick={() => moveSlideFunding(+1)}>다음</ButtonItemButton>
+					<ButtonItemButton onClick={() => moveSlideFunding(-1)}>&lt;</ButtonItemButton>
+					<ButtonItemButton onClick={() => moveSlideFunding(+1)}>&gt;</ButtonItemButton>
 				</div>
 			</ButtonDiv>
 			<FachiveFundingRecommend>
@@ -108,7 +112,14 @@ const ButtonItemButton = styled.button`
 	margin-left: 10px;
 	margin-top: 30px;
 	margin-bottom: 10px;
-	font-weight: 800;
+	font-weight: 600;
+	font-size: 16px;
+	background-color: white;
+	border: none;
+	width: 8px;
+	height: 16px;
+	margin-left: 20px;
+	padding-right: 30px;
 `;
 
 const RecommendButton = styled.button`
@@ -118,18 +129,21 @@ const RecommendButton = styled.button`
 	padding: 15px;
 	font-size: 16px;
 	font-weight: 700;
+	text-decoration: none;
 	cursor: pointer;
 `;
 const RecommendFlexDiv = styled.div`
 	display: flex;
 	gap: 35px;
 `;
+
 const RecommendTitle = styled.div`
 	margin-left: 10px;
 	margin-top: 30px;
 	margin-bottom: 10px;
 	font-weight: 800;
 `;
+
 const FundingImg = styled.img`
 	width: 100%;
 	height: 295px;
@@ -141,17 +155,21 @@ const FundingImg = styled.img`
 	border-radius: 30px;
 	background-position: center;
 `;
+
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 `;
+
 const FachiveFashionRecommend = styled.div`
 	overflow: hidden;
 `;
+
 const FachiveFundingRecommend = styled.div`
 	overflow: hidden;
 	margin-bottom: 100px;
 `;
+
 const SlideImg = styled.img`
 	max-width: 100%;
 	height: 500px;
