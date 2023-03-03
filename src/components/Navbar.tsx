6b402dd46000901bc9bf66947092d19d19c31cb3
vjvl95 <vjvl95@naver.com>
2023-03-03 14:33:04 +0900

    refactor: 로그아웃 시 홈으로 이동

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import LogoImg from '../assets/Logo.png';
 import { NAV_ITEM } from '../constants/editor';
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 const Navbar = () => {
 	const location = useLocation();
+	const nav = useNavigate();
+
 	const [isLogin, setIsLogin] = useState(false);
 	useEffect(() => {
 		if (sessionStorage.getItem('token') !== null) {
@@ -18,6 +20,7 @@ const Navbar = () => {
 		sessionStorage.removeItem('displayName');
 		sessionStorage.removeItem('email');
 		sessionStorage.removeItem('token');
+		nav('/');
 	}
 	return (
 		<Container>
@@ -43,7 +46,11 @@ const Navbar = () => {
 			</NavContainerDiv>
 			<UserContainerDiv>
 				{isLogin ? (
-					<UserItemDiv onClick={logoutHandler}>로그아웃</UserItemDiv>
+					<div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+						<WritePostButton onClick={() => nav('/newpost')}>게시글 작성하기</WritePostButton>
+						<UserDropBoxDiv></UserDropBoxDiv>
+						<UserItemDiv onClick={logoutHandler}>로그아웃</UserItemDiv>
+					</div>
 				) : (
 					<>
 						<Link style={{ textDecorationLine: 'none', color: 'black' }} to="/login">
@@ -62,6 +69,23 @@ const Navbar = () => {
 
 export default Navbar;
 
+const UserDropBoxDiv = styled.div`
+	width: 30px;
+	height: 30px;
+	background-color: #d9d9d9;
+	border-radius: 30px;
+`;
+
+const WritePostButton = styled.button`
+	background-color: white;
+	border: 1px solid #cacaca;
+	cursor: pointer;
+	color: #999999;
+	font-size: 13px;
+	font-weight: 700;
+	padding: 15px;
+	border-radius: 30px;
+`;
 const Container = styled.div`
 	width: 100%;
 	height: 60px;
