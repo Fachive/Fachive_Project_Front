9a3673db25c2ef39d141744b42d0c5eb36da48d9
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2023-03-05 22:15:00 +0900

    feat: 로그인 감지 후 접근 불가능한 페이지에서 main 으로 라우팅 처리

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,17 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import { Home, Fashion, Detail, Login, Sign, Editor, Portfolio } from '../pages';
 import Profile from '../pages/Profile';
 
 const Router = () => {
+	const sessionStorageDisplayName = window.sessionStorage.getItem('displayName');
+	const sessionStorageToken = window.sessionStorage.getItem('token');
+	const navigate = useNavigate();
+	const location = useLocation();
+	useEffect(() => {
+		if (!sessionStorageDisplayName && !sessionStorageToken && location.pathname.includes('profile')) {
+			navigate('/');
+		} else if (sessionStorageDisplayName || (sessionStorageToken && location.pathname.includes('login'))) {
+			navigate('/');
+		} else if (sessionStorageDisplayName || (sessionStorageToken && location.pathname.includes('sign'))) {
+			navigate('/');
+		}
+	}, [location.pathname]);
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
 				<Route index element={<Home />} />
 				<Route path="/fashion/detail/:id" element={<Detail />} />
 				<Route path="/funding/detail/:id" element={<Detail />} />
-
-				<Route path="/login" element={<Login />} />
 				<Route path="/fashion" element={<Fashion />} />
 				<Route path="/funding" element={<Fashion />} />
 				<Route path="/newpost" element={<Editor />} />
